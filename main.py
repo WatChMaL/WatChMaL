d2d5eef366956f40b16ec89c89571ea2029eb7f4
Whisky-Jack <joshtindall1@gmail.com>
2020-10-02 15:37:31 -0700

    started implementing actual multiprocessing

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from hydra.utils import instantiate
 # torch imports
 import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.multiprocessing as mp
 
 # WatChMaL imports
 from watchmal.dataset.data_utils import get_data_loader
+
+# generic imports
+import os
 
 logger = logging.getLogger('train')
 
@@ -18,23 +22,39 @@ def main(config):
     logger.info(f"Running with the following config:\n{OmegaConf.to_yaml(config)}")
 
     #TODO: is this needed
-    ngpus_per_node = torch.cuda.device_count()
+    #ngpus_per_node = torch.cuda.device_count()
+    ngpus = len(config.gpu_list)
+    
+    # TODO: initialize process group env variables
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
 
-    if (len(config.gpu_list)) > 1:
+    if ngpus > 1:
         print("Using multiprocessing")
         print("Requesting GPUs. GPU list : " + str(config.gpu_list))
         devids = ["cuda:{0}".format(x) for x in config.gpu_list]
-        print("Using DistributedDataParallel on these devices: {}".format.devids)
+        print("Using DistributedDataParallel on these devices: {}".format(devids))
+        mp.spawn(main_worker_function, nprocs=ngpus, args=(ngpus, config))
     else:
         print("Only one gpu found")
         gpu = config.gpu_list[0]
-        main_worker_function(gpu, ngpus_per_node, config)
+        main_worker_function(gpu, ngpus, config)
 
 def main_worker_function(gpu, ngpus_per_node, config):
     print("Main loop")
+    # TODO: how should this interact with self.device
+    torch.cuda.set_device(gpu)
+
+    world_size = ngpus_per_node
+    torch.distributed.init_process_group(
+        'nccl',
+        init_method='env://',
+        world_size=world_size,
+        rank=gpu,
+    )
 
     # Instantiate model and engine
-    model = instantiate(config.model)
+    model = instantiate(config.model).to(gpu)
 
     # configure the device to be used for model training and inference
     if len(config.gpu_list) > 1:
