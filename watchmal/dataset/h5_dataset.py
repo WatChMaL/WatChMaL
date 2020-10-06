fixed name scheme for h5 files and streamlined length calculation

watchmal/dataset/h5_dataset.py
@@ -10,27 +10,26 @@ class H5Dataset(Dataset, ABC):
     def __init__(self, h5_path, transforms=None):
         self.h5_path = h5_path
 
-        file_descriptor = open(self.h5_path, 'rb')
-        init_h5_file = h5py.File(file_descriptor, "r")
-
-        self.dataset_length = np.array(init_h5_file["labels"]).shape[0]
+        with h5py.File(self.h5_path, 'r') as init_h5_file:
+            self.dataset_length = init_h5_file["labels"].shape[0]
 
     def open_hdf5(self):
         """
         # TODO: This is needed for multiprocessing
         """
-        file_descriptor = open(h5_path, 'rb')
-        self.h5_file = h5py.File(file_descriptor, "r")
+        file_descriptor = open(self.h5_path, 'rb')
+        h5_file = h5py.File(file_descriptor, "r")
 
         # Create a memory map for event_data - loads event data into memory only on __getitem__()
         hdf5_hit_pmt = h5_file["hit_pmt"]
         hdf5_hit_charge = h5_file["hit_charge"]
-        self.hit_pmt = np.memmap(h5_path, mode="r", shape=hdf5_hit_pmt.shape, offset=hdf5_hit_pmt.id.get_offset(),
+
+        self.hit_pmt = np.memmap(self.h5_path, mode="r", shape=hdf5_hit_pmt.shape, offset=hdf5_hit_pmt.id.get_offset(),
                                  dtype=hdf5_hit_pmt.dtype)
-        self.time = np.memmap(h5_path, mode="r", shape=h5_file["hit_time"].shape,
+        self.time = np.memmap(self.h5_path, mode="r", shape=h5_file["hit_time"].shape,
                               offset=h5_file["hit_time"].id.get_offset(),
                               dtype=h5_file["hit_time"].dtype)
-        self.charge = np.memmap(h5_path, mode="r", shape=hdf5_hit_charge.shape,
+        self.charge = np.memmap(self.h5_path, mode="r", shape=hdf5_hit_charge.shape,
                                 offset=hdf5_hit_charge.id.get_offset(), dtype=hdf5_hit_charge.dtype)
 
         # Load the contents which could fit easily into memory
@@ -41,6 +40,9 @@ class H5Dataset(Dataset, ABC):
         self.event_hits_index = np.append(h5_file["event_hits_index"], self.hit_pmt.shape[0]).astype(np.int64)
         self.event_ids = np.array(h5_file["event_ids"])
         self.root_files = np.array(h5_file["root_files"])
+
+        # create attribute so that method won't be invoked again
+        self.h5_file = h5_file
 
     @abstractmethod
     def get_data(self, hit_pmts, hit_charges, hit_times):