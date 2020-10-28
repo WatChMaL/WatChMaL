a950657cbcf9d3d07b3c943cabd4b26dbdf83e23
Whisky-Jack <joshtindall1@gmail.com>
2020-10-28 11:36:22 -0700

    fixed output collapsing in plotting

--- analysis/comparison_utils.py
+++ analysis/comparison_utils.py
@@ -31,7 +31,7 @@ def multi_get_masked_data(data_list, cut_path, test_idxs, cut_list):
     output_data_list = [np.delete(data, cut_idxs, 0) for data in data_list]
     return output_data_list
 
-def collapse_test_output(softmaxes, labels, index_dict,predictions=None,ignore_type=None):
+def collapse_test_output(softmaxes, labels, index_dict, predictions=None,ignore_type=None):
     '''
     Collapse gamma class into electron class to allow more equal comparison to FiTQun.
     Args:
@@ -47,13 +47,14 @@ def collapse_test_output(softmaxes, labels, index_dict,predictions=None,ignore_t
         keep_indices = np.where(labels!=index_dict[ignore_type])[0]
         softmaxes = softmaxes[keep_indices]
         labels = labels[keep_indices]
-        if predictions is not None: predictions = predictions[keep_indices]
+        if predictions is not None:
+            predictions = predictions[keep_indices]
 
     new_labels = np.ones((softmaxes.shape[0]))*index_dict['$e$']
     new_softmaxes = np.zeros((labels.shape[0], 3))
 
     if predictions is not None:
-        new_predictions = np.ones_like(predictions) * index_dict['$e$']
+        new_predictions = np.ones_like(predictions) * 1 #index_dict['$e$']
     
     for idx, label in enumerate(labels):
             if index_dict["$\mu$"] == label: new_labels[idx] = index_dict["$\mu$"]
@@ -66,11 +67,11 @@ def collapse_test_output(softmaxes, labels, index_dict,predictions=None,ignore_t
     return new_softmaxes, new_labels
 
 
-def multi_collapse_test_output(output_softmax_list, actual_labels_list, label_dict):
+def multi_collapse_test_output(output_softmax_list, actual_labels_list, label_dict, ignore_type=None):
     collapsed_class_scores_list, collapsed_class_labels_list = [],[]
 
     for softmaxes, labels in zip(output_softmax_list, actual_labels_list):
-        collapsed_class_scores, collapsed_class_labels = collapse_test_output(softmaxes, labels, label_dict)
+        collapsed_class_scores, collapsed_class_labels = collapse_test_output(softmaxes, labels, label_dict, ignore_type=ignore_type)
         collapsed_class_scores_list.append(collapsed_class_scores)
         collapsed_class_labels_list.append(collapsed_class_labels)
 
