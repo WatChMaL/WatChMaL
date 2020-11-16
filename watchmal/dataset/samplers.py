7a02d8883088292630c0ef00de06559c08fe0046
Whisky-Jack <joshtindall1@gmail.com>
2020-11-16 09:39:28 -0800

    fixing distributedsampler wrapper

--- watchmal/dataset/samplers.py
+++ watchmal/dataset/samplers.py
@@ -9,10 +9,104 @@ def SubsetSequentialSampler(indices):
     return indices
 
 
+class DistributedSamplerWrapper(DistributedSampler):
+    """
+    Wrapper over `Sampler` for distributed training.
+    Allows you to use any sampler in distributed mode.
+    It is especially useful in conjunction with
+    `torch.nn.parallel.DistributedDataParallel`. In such case, each
+    process can pass a DistributedSamplerWrapper instance as a DataLoader
+    sampler, and load a subset of subsampled data of the original dataset
+    that is exclusive to it.
+    .. note::
+        Sampler is assumed to be of constant size.
+    """
+
+    def __init__(
+        self,
+        sampler,
+        seed,
+        num_replicas: Optional[int] = None,
+        rank: Optional[int] = None,
+        shuffle: bool = False,
+    ):
+        """
+        Args:
+            sampler: Sampler used for subsampling
+            num_replicas (int, optional): Number of processes participating in
+              distributed training
+            rank (int, optional): Rank of the current process
+              within ``num_replicas``
+            shuffle (bool, optional): If true,
+              sampler will shuffle the indices
+        """
+        super(DistributedSamplerWrapper, self).__init__(
+            DatasetFromSampler(sampler, seed),
+            num_replicas=num_replicas,
+            rank=rank,
+            shuffle=shuffle,
+            seed=seed
+        )
+        self.sampler = sampler
+    
+    def set_epoch(self, epoch):
+        self.epoch = epoch
+
+    def __iter__(self):
+        """@TODO: Docs. Contribution is welcome."""
+        updated_seed = self.seed + int(self.epoch)
+        self.dataset = DatasetFromSampler(self.sampler, seed=updated_seed)
+        indexes_of_indexes = super().__iter__()
+        subsampler_indexes = self.dataset
+        return iter(itemgetter(*indexes_of_indexes)(subsampler_indexes))
+
+class DatasetFromSampler(Dataset):
+    """Dataset of indexes from `Sampler`."""
+
+    def __init__(self, sampler: Sampler, seed: int):
+        """
+        Args:
+            sampler: @TODO: Docs. Contribution is welcome
+        """
+        self.sampler = sampler
+        self.seed = seed
+        self.sampler_list = None
+
+    def __getitem__(self, index: int):
+        """Gets element of the dataset.
+        Args:
+            index: index of the element in the dataset
+        Returns:
+            Single element by index
+        """
+        if self.sampler_list is None:
+            torch.manual_seed(self.seed)
+            self.sampler_list = list(self.sampler)
+        return self.sampler_list[index]
+
+    def __len__(self) -> int:
+        """
+        Returns:
+            int: length of the dataset
+        """
+        return len(self.sampler)
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 Wrapper borrowed from https://github.com/catalyst-team/catalyst/blob/master/catalyst/data/sampler.py
 """
-class DistributedSamplerWrapper(DistributedSampler):
+class DeprecatedDistributedSamplerWrapper(DistributedSampler):
     """
     Wrapper over `Sampler` for distributed training.
     Allows you to use any sampler in distributed mode.
