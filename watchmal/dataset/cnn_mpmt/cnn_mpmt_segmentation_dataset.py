06a2ac0d6bb312e10ca4d3f1a292c4fa0693c269
Nick Prouse <nprouse@triumf.ca>
2021-02-23 21:11:16 -0800

    Fix code to calculate parent, to use either the only parent or the max parent, chosen by config option

--- watchmal/dataset/cnn_mpmt/cnn_mpmt_segmentation_dataset.py
+++ watchmal/dataset/cnn_mpmt/cnn_mpmt_segmentation_dataset.py
@@ -18,7 +18,7 @@ import watchmal.dataset.data_utils as du
 
 class CNNmPMTSegmentationDataset(Dataset):
     def __init__(self, digi_dataset_config, true_hits_h5file, digi_truth_mapping_file, valid_parents=(-1, 2, 3),
-                 transform_segmentation = True):
+                 parent_type="max", transform_segmentation = True):
         """
         Args:
             digi_dataset_config     ... config for dataset for digitized hits
@@ -36,23 +36,49 @@ class CNNmPMTSegmentationDataset(Dataset):
         self.truth_dataset = H5TrueDataset(true_hits_h5file, transforms=None, digitize_hits=False)
         with open(digi_truth_mapping_file, 'rb') as f:
             self.digi_truth_mapping = pickle.load(f)
-        self.valid_parents = valid_parents
+        self.valid_parents = np.array(valid_parents)
+        if parent_type=="only":
+            self.get_digi_hit_parent = self.get_digi_hit_only_parent
+        elif parent_type=="max":
+            self.get_digi_hit_parent = self.get_digi_hit_max_parent
 
-    def get_digi_hit_parent(self, digi_hit_pmt, true_hit_pmt, true_hit_parent):
-        digi_hit_parent_count = {}
-        for p in self.valid_parents:
+    def get_digi_hit_only_parent(self, digi_hit_pmt, true_hit_pmt, true_hit_parent):
+        # find which digi hits have which parents
+        digi_hit_has_parent = np.zeros((len(self.valid_parents), len(digi_hit_pmt)))
+        for i, p in enumerate(self.valid_parents):
             parent_true_hits = np.where(true_hit_parent == p)
             parent_hit_pmts = true_hit_pmt[parent_true_hits]
-            digi_hit_parent_count[p] = np.isin(digi_hit_pmt, parent_hit_pmts)
-        digi_hit_parent = np.zeros(digi_hit_pmt.shape)
-        for p in self.valid_parents:
-            is_this_parent = np.ones(digi_hit_pmt.shape)
-            for o in self.valid_parents:
-                if o == p:
-                    is_this_parent &= digi_hit_parent_count[p]
-                else:
-                    is_this_parent &= ~digi_hit_parent_count[p]
-            digi_hit_parent[is_this_parent] = p
+            digi_hit_has_parent[i] = np.isin(digi_hit_pmt, parent_hit_pmts)
+        # choose the hit parent if it only has one parent, or -2 if it has multiple
+        # first take the argmax (will be the only parent if there's only one parent, or first parent if there's multiple parents)
+        digi_hit_parent = self.valid_parents[np.argmax(digi_hit_has_parent, axis=0)]
+        # replace with -2 for any with more than one parent
+        digi_hit_parent[np.where(np.count_nonzero(digi_hit_has_parent, axis=0)>1)] = -2
+        return digi_hit_parent
+
+    def get_digi_hit_max_parent(self, digi_hit_pmt, true_hit_pmt, true_hit_parent):
+        # sort the digi hit pmts for fast processing
+        sort = np.argsort(digi_hit_pmt)
+        unsort = np.empty(sort.shape, dtype=int)
+        unsort[sort] = np.arange(len(sort))
+        digi_hit_pmt_sorted = digi_hit_pmt[sort]
+        # find the (sorted) index of the digi hit for each true hit, and mask those that have no digi hit
+        true_hit_sorted_digi_index = np.searchsorted(digi_hit_pmt_sorted, true_hit_pmt)
+        true_hit_sorted_digi_index[true_hit_sorted_digi_index == len(digi_hit_pmt)] = 0
+        mask = digi_hit_pmt_sorted[true_hit_sorted_digi_index] == true_hit_pmt
+        # count the number of true hits of given parent type for each digihit
+        digi_hit_parent_count = np.zeros((len(self.valid_parents), len(digi_hit_pmt)))
+        for i, p in enumerate(self.valid_parents):
+            parent_hits_sorted_digi_index = true_hit_sorted_digi_index[mask & (true_hit_parent == p)]
+            sorted_counts = np.zeros(digi_hit_pmt.shape, dtype=int)
+            np.add.at(sorted_counts, parent_hits_sorted_digi_index, 1)
+            digi_hit_parent_count[i] = sorted_counts[unsort]
+        # choose the hit parent based on which parent has the highest count
+        count_argmax = np.argmax(digi_hit_parent_count, axis=0)
+        digi_hit_parent = self.valid_parents[count_argmax]
+        # replace with -2 for any digi hits with equal max count of more than one parent
+        count_max = digi_hit_parent_count[count_argmax, np.arange(len(count_argmax))]
+        digi_hit_parent[np.where(np.count_nonzero(digi_hit_parent_count==count_max, axis=0)>1)] = -2
         return digi_hit_parent
 
     def __getitem__(self, item):
