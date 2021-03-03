9910a402c6c815e75313e389b27ec61aaccfe7d6
agoryelov <andrey.goryelov@gmail.com>
2021-03-03 15:09:57 -0800

    Added log scale

--- graph.py
+++ graph.py
@@ -1,5 +1,9 @@
+from analysis.plot_utils import disp_reg_hist
+
+_OUTPUT_PATH = './outputs/2021-02-28/20-07-49/outputs'
+
 def main():
-  pass
+  disp_reg_hist(_OUTPUT_PATH)
 
 if __name__ == "__main__":
   main()
