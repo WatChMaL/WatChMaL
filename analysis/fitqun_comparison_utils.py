7f06413a52d5a5881696569538a65be166ef9d18
Whisky-Jack <joshtindall1@gmail.com>
2021-03-08 10:37:25 -0800

    added updated fitqun loading function

--- analysis/fitqun_comparison_utils.py
+++ analysis/fitqun_comparison_utils.py
@@ -3,12 +3,14 @@ Utils for comparing performance to fiTQun
 """
 
 import numpy as np
+import pickle
+import uproot
 import h5py
 import re
 from functools import reduce
 from tqdm import tqdm_notebook as tqdm
 
-def load_fq_output(mapping_indices_path, fq_failed_idxs_path, test_idxs_path, cut_path, cut_list):
+def load_fq_output(fq_mapping_path, gamma_file_path, e_file_path, mu_file_path):
     # TODO: rework to fit current framework
     '''
     load_fq_output(mapping_indices_path, test_idxs, cut_path, cut_list)
@@ -25,94 +27,60 @@ def load_fq_output(mapping_indices_path, fq_failed_idxs_path, test_idxs_path, cu
     '''
     ###### Load the fiTQun results ######
 
-    # File paths for fiTQun results
-    fiTQun_gamma_path = "/fast_scratch/WatChMaL/data/IWCDmPMT_4pi_fulltank_fiTQun_gamma.npz"
-    fiTQun_e_path     = "/fast_scratch/WatChMaL/data/IWCDmPMT_4pi_fulltank_fiTQun_e-.npz"
-    fiTQun_mu_path    = "/fast_scratch/WatChMaL/data/IWCDmPMT_4pi_fulltank_fiTQun_mu-.npz"
+    with open('./fq_comparison_data/3M_fitqun_mapping.pickle', 'rb') as handle:
+        fq_mapping = pickle.load(handle)
+
+    gamma_fq_indices = fq_mapping['gamma_fq_indices']
+    e_fq_indices     = fq_mapping['e_fq_indices']
+    mu_fq_indices    = fq_mapping['mu_fq_indices']
 
     # Load fiTQun results
-    f_gamma = np.load(fiTQun_gamma_path, allow_pickle=True)
-    f_e     = np.load(fiTQun_e_path, allow_pickle=True)
-    f_mu    = np.load(fiTQun_mu_path, allow_pickle=True)
+    gamma_file_data = uproot.open(gamma_file_path)['fiTQun;1']
+    e_file_data     = uproot.open(e_file_path)['fiTQun;1']
+    mu_file_data    = uproot.open(mu_file_path)['fiTQun;1']
 
-    fq_filename_original = (f_gamma['filename'], f_e['filename'], f_mu['filename'])
-    fq_eventids_original = (f_gamma['eventid'], f_e['eventid'], f_mu['eventid'])
-    fq_flag_original     = (f_gamma['flag'], f_e['flag'], f_mu['flag'])
-    fq_nll_original      = (f_gamma['nLL'], f_e['nLL'], f_mu['nLL'])
-    fq_mom_original      = (f_gamma['momentum'],f_e['momentum'],f_mu['momentum'])
+    # Load gamma results
+    gamma_set_nll    = gamma_file_data.arrays('fq1rnll')['fq1rnll']
 
-    n_events = int(reduce(lambda x,y : x+y, list(map(lambda x : x.shape[0], fq_filename_original))))
+    gamma_set_e_nll  = gamma_set_nll[:, 0, 1]
+    gamma_set_mu_nll = gamma_set_nll[:, 0, 2]
 
-    fq_rootfiles = np.empty(n_events,dtype=object)
-    fq_eventids  = np.zeros(n_events)
-    fq_flag      = np.empty((n_events, 2))
-    fq_nll       = np.empty((n_events, 2))
-    fq_mom       = np.empty((n_events, 2))
+    gamma_set_discriminator = np.array(gamma_set_mu_nll - gamma_set_e_nll)
 
-    fq_mapping_indices = np.load(mapping_indices_path,allow_pickle=True)['arr_0']
+    # Load electron results
+    e_set_nll    = e_file_data.arrays('fq1rnll')['fq1rnll']
 
-    fq_failed_idxs = np.load(fq_failed_idxs_path, allow_pickle = True)['failed_indices_pointing_to_h5_test_set'].astype(int)
-    filtered_indices = np.load("/fast_scratch/WatChMaL/data/IWCD_fulltank_300_pe_idxs.npz", allow_pickle=True)
-    test_filtered_indices = filtered_indices['test_idxs']
-    stest_filtered_indices = np.delete(test_filtered_indices, fq_failed_idxs,0)
+    e_set_e_nll  = e_set_nll[:, 0, 1]
+    e_set_mu_nll = e_set_nll[:, 0, 2]
 
-    idx_dic = {}
-    for i, idx in enumerate(stest_filtered_indices):
-        idx_dic[idx] = i
+    e_set_discriminator = np.array(e_set_mu_nll - e_set_e_nll)
 
-    test_idxs = np.load(test_idxs_path, allow_pickle=True)['test_idxs']
+    # Load mu results
+    mu_set_nll    = mu_file_data.arrays('fq1rnll')['fq1rnll']
 
-    keep_idxs = []
-    stest_idxs = []
-    i=0
-    for idx in test_idxs:
-        try:
-            keep_idxs.append(idx_dic[idx])
-            stest_idxs.append(idx)
-        except KeyError:
-            i+=1
+    mu_set_e_nll  = mu_set_nll[:, 0, 1]
+    mu_set_mu_nll = mu_set_nll[:, 0, 2]
 
-    f = h5py.File("/fast_scratch/WatChMaL/data/IWCDmPMT_4pi_full_tank_pointnet.h5", "r")
-    original_labels = np.array(f['labels'])
-    labels = np.array(original_labels[test_filtered_indices])
-    f.close()
-    slabels = np.delete(labels, fq_failed_idxs, 0)
+    mu_set_discriminator = np.array(mu_set_mu_nll - mu_set_e_nll) 
 
-    for i,ptype in enumerate(slabels):
-        fq_rootfiles[i] = str(fq_filename_original[ptype][fq_mapping_indices[i]])
-        fq_eventids[i] = fq_eventids_original[ptype][fq_mapping_indices[i]]
-        fq_flag[i] = fq_flag_original[ptype][fq_mapping_indices[i]]
-        fq_nll[i] = fq_nll_original[ptype][fq_mapping_indices[i]]
-        fq_mom[i] = fq_mom_original[ptype][fq_mapping_indices[i]]
-    
-    fq_scores = np.zeros((fq_nll.shape[0],3))
-    fq_scores[:,0] = fq_nll[:,1] - fq_nll[:,0]
-    fq_scores[:,1] = fq_nll[:,1] - fq_nll[:,0]
-    fq_scores[:,2] = fq_nll[:,0] - fq_nll[:,1]
-    fq_labels = slabels
 
-    fq_rootfiles = fq_rootfiles[keep_idxs]
-    fq_eventids = fq_eventids[keep_idxs]
-    fq_flag = fq_flag[keep_idxs]
-    fq_scores = fq_scores[keep_idxs]
-    fq_mom = fq_mom[keep_idxs]
-    fq_labels = fq_labels[keep_idxs]
+    # Construct likelihoods
+    fq_likelihoods = np.concatenate((e_set_discriminator[e_fq_indices],
+                                     gamma_set_discriminator[gamma_fq_indices],
+                                     mu_set_discriminator[mu_fq_indices]
+                                     ))
 
-    fq_rootfiles = apply_cuts(fq_rootfiles, stest_idxs, cut_path, cut_list)
-    fq_eventids = apply_cuts(fq_eventids, stest_idxs, cut_path, cut_list)
-    fq_flag = apply_cuts(fq_flag, stest_idxs, cut_path, cut_list)
-    fq_scores = apply_cuts(fq_scores, stest_idxs, cut_path, cut_list)
-    fq_mom = apply_cuts(fq_mom, stest_idxs, cut_path, cut_list)
-    fq_labels = apply_cuts(fq_labels, stest_idxs, cut_path, cut_list)
+    # Generate scores
+    fq_scores = np.zeros((fq_likelihoods.shape[0], 3))
+    fq_scores[:, 1] = fq_likelihoods
 
-    fq_outputs = {'fq_rootfiles':fq_rootfiles, 
-                  'fq_eventids':fq_eventids, 
-                  'fq_flag':fq_flag, 
-                  'fq_scores':fq_scores, 
-                  'fq_mom':fq_mom, 
-                  'fq_labels':fq_labels}
+    # Generate labels
+    fq_labels = np.concatenate((np.ones_like(e_set_discriminator[e_fq_indices])*1,
+                                np.ones_like(gamma_set_discriminator[gamma_fq_indices])*0,
+                                np.ones_like(mu_set_discriminator[mu_fq_indices])*2
+                                ))
 
-    return fq_outputs
+    return fq_scores, fq_labels
 
 def deprecated_load_fq_output(mapping_indices_path, fq_failed_idxs_path, test_idxs_path, cut_path, cut_list):
     # TODO: rework to fit current framework
@@ -220,7 +188,7 @@ def deprecated_load_fq_output(mapping_indices_path, fq_failed_idxs_path, test_id
 
     return fq_outputs
 
-def verify_fq_matching(fq_rootfiles, fq_eventids, filtered_dataset_rootfiles, filtered_dataset_eventids):
+def deprecated_verify_fq_matching(fq_rootfiles, fq_eventids, filtered_dataset_rootfiles, filtered_dataset_eventids):
     description = 'Verification Progress: '
 
     for i in tqdm(range(len(fq_rootfiles)), desc=description, position=pos):
