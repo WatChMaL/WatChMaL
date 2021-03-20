bc6d0b3e47fc0b9cf9f6d62754b38d3a55161bf0
Nick Prouse <nprouse@triumf.ca>
2021-03-19 18:57:41 -0700

    Add option to disable using times, and fix transformations when using orientations

--- watchmal/dataset/pointnet/pointnet_dataset.py
+++ watchmal/dataset/pointnet/pointnet_dataset.py
@@ -15,34 +15,39 @@ import watchmal.dataset.data_utils as du
 
 class PointNetDataset(H5Dataset):
 
-    def __init__(self, h5file, geometry_file, is_distributed, use_orientations=False, n_points=4000, transforms=None):
+    def __init__(self, h5file, geometry_file, is_distributed, use_times=True, use_orientations=False, n_points=4000, transforms=None):
         super().__init__(h5file, is_distributed)
         geo_file = np.load(geometry_file, 'r')
         self.geo_positions = torch.from_numpy(geo_file["position"]).float()
         self.geo_orientations = torch.from_numpy(geo_file["orientation"]).float()
         self.use_orientations = use_orientations
+        self.use_times = use_times
         self.n_points = n_points
         self.transforms = du.get_transformations(transformations, transforms)
+        self.channels = 4
+        if use_orientations:
+            self.channels += 3
+        if use_times:
+            self.channels += 1
 
 
     def  __getitem__(self, item):
 
         data_dict = super().__getitem__(item)
         hit_pmts = data_dict["data"]["hit_pmts"]
-        hit_charges = data_dict["data"]["hit_charges"]
-        hit_times = data_dict["data"]["hit_times"]
 
-        hit_positions = self.geo_positions[hit_pmts, :]
         n_hits = min(self.n_points, hit_pmts.shape[0])
-        if not self.use_orientations:
-            data = np.zeros((5, self.n_points))
-        else:
+        hit_positions = self.geo_positions[hit_pmts[:n_hits], :]
+        hit_charges = data_dict["data"]["hit_charges"][:n_hits]
+        data = np.zeros((self.channels, self.n_points))
+        data[:3, :n_hits] = hit_positions.T
+        if self.use_orientations:
             hit_orientations = self.geo_orientations[hit_pmts[:n_hits], :]
-            data = np.zeros((7, self.n_points))
-            data[3:5, :n_hits] = hit_orientations.T
-        data[:3, :n_hits] = hit_positions[:n_hits].T
-        data[-2, :n_hits] = hit_charges[:n_hits]
-        data[-1, :n_hits] = hit_times[:n_hits]
+            data[3:6, :n_hits] = hit_orientations.T
+        if self.use_times:
+            hit_times = data_dict["data"]["hit_times"][:n_hits]
+            data[-2, :n_hits] = hit_times
+        data[-1, :n_hits] = hit_charges
 
         data = du.apply_random_transformations(self.transforms, data)
 
