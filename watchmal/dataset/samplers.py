8ca648d8e0802da8e73947873455f08e9d63b8c2
Ben Huckell <b.huckell72@gmail.com>
2021-05-10 18:58:51 -0700

    Fixing some discrepancies

--- watchmal/dataset/samplers.py
+++ watchmal/dataset/samplers.py
@@ -1,9 +1,16 @@
-from operator import itemgetter
-from typing import Optional
+"""
+Sampler classes
+"""
 
+# torch imports
 import torch
 from torch.utils.data import Dataset, Sampler
 from torch.utils.data.distributed import DistributedSampler
+
+# generic imports
+from operator import itemgetter
+from typing import Optional
+import time
 
 def SubsetSequentialSampler(indices):
     return indices
@@ -11,15 +18,13 @@ def SubsetSequentialSampler(indices):
 
 class DistributedSamplerWrapper(DistributedSampler):
     """
-    Wrapper over `Sampler` for distributed training.
-    Allows you to use any sampler in distributed mode.
-    It is especially useful in conjunction with
-    `torch.nn.parallel.DistributedDataParallel`. In such case, each
-    process can pass a DistributedSamplerWrapper instance as a DataLoader
-    sampler, and load a subset of subsampled data of the original dataset
-    that is exclusive to it.
-    .. note::
-        Sampler is assumed to be of constant size.
+    Wrapper for making general samplers compatible with multiprocessing.
+
+    Allows you to use any sampler in distributed mode when training with 
+    torch.nn.parallel.DistributedDataParallel. In such case, each process 
+    can pass a DistributedSamplerWrapper instance as a DataLoader sampler, 
+    and load a subset of subsampled data of the original dataset that is 
+    exclusive to it.
     """
 
     def __init__(
@@ -32,13 +37,10 @@ class DistributedSamplerWrapper(DistributedSampler):
     ):
         """
         Args:
-            sampler: Sampler used for subsampling
-            num_replicas (int, optional): Number of processes participating in
-              distributed training
-            rank (int, optional): Rank of the current process
-              within ``num_replicas``
-            shuffle (bool, optional): If true,
-              sampler will shuffle the indices
+            sampler                         ... Sampler used for subsampling
+            num_replicas (int, optional)    ... Number of processes participating in distributed training
+            rank (int, optional)            ... Rank of the current process within ``num_replicas``
+            shuffle (bool, optional)        ... If true sampler will shuffle the indices
         """
         super(DistributedSamplerWrapper, self).__init__(
             list(sampler),
@@ -52,7 +54,7 @@ class DistributedSamplerWrapper(DistributedSampler):
     
     def set_epoch(self, epoch):
         self.epoch = epoch
-
+    
     def __iter__(self):
         # fetch DistributedSampler indices
         indexes_of_indexes = super().__iter__()
