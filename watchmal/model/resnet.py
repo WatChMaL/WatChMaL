72389ea000ccbc26eca29b50ec31fb0a5040d7a8
lbidulka <lukebidulka@gmail.com>
2022-08-05 09:45:25 -0700

    Cleaned Comments

--- watchmal/model/resnet.py
+++ watchmal/model/resnet.py
@@ -21,20 +21,11 @@ def conv4x4(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=4, stride=stride, padding=1, bias=False)
 
 
-# +
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
-
-#         if downsample is None:
-#             self.conv1 = conv3x3(inplanes, planes, stride)
-#         else:
-#             if planes < 512:
-#                 self.conv1 = conv4x4(inplanes, planes, stride=2)
-#             else:
-#                 self.conv1 = conv2x2(inplanes, planes)
         
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -63,14 +54,11 @@ class BasicBlock(nn.Module):
         return out
 
 
-# +
 class Bottleneck(nn.Module):
     expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-
-#         downsample=None
         
         self.conv1 = conv1x1(inplanes, planes)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -112,7 +100,6 @@ class Bottleneck(nn.Module):
         return out
 
 
-# +
 class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_input_channels, num_output_channels, zero_init_residual=False):
@@ -123,14 +110,9 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(num_input_channels, 64, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        
-        #self.conv2 = nn.Conv2d(64, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        #self.bn2 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
-        
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        #self.layer0 = BasicBlock(64, 64)
         self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
@@ -140,17 +122,6 @@ class ResNet(nn.Module):
         
         self.unroll_size = 512 * block.expansion
         self.bool_deep = False
-
-        #self.conv3a = nn.Conv2d(self.unroll_size, self.unroll_size, kernel_size=(4, 4), stride=(1, 1))
-        #self.conv3b = nn.Conv2d(self.unroll_size, self.unroll_size, kernel_size=(1, 4), stride=(1, 1))
-        #self.conv3c = nn.Conv2d(self.unroll_size, self.unroll_size, kernel_size=(2, 2), stride=(1, 1))
-        #self.conv3d = nn.Conv2d(self.unroll_size, self.unroll_size, kernel_size=(1, 2), stride=(1, 1))
-        #self.conv3e = nn.Conv2d(self.unroll_size, self.unroll_size, kernel_size=(9, 9), stride=(1, 1))
-        
-        #padding
-        #self.conv3f = nn.Conv2d(self.unroll_size, self.unroll_size, kernel_size=(1, 3), stride=(1, 1))
-
-        #self.bn3 = nn.BatchNorm2d(self.unroll_size)
 
         for m in self.modules():
             if isinstance(m, Bottleneck):
@@ -194,70 +165,23 @@ class ResNet(nn.Module):
             layers.append(block(self.inplanes, planes,))
 
         return nn.Sequential(*layers)
-    
-#     def _make_layer(self, block, planes, blocks, stride=1):
-#         downsample = None
-#         if stride != 1 or self.inplanes != planes * block.expansion:
-#             if planes < 512:
-#                 downsample = nn.Sequential(
-#                     conv4x4(self.inplanes, planes * block.expansion, stride),
-#                     nn.BatchNorm2d(planes * block.expansion),
-#                 )
-#             else:
-#                 downsample = nn.Sequential(
-#                     conv2x2(self.inplanes, planes * block.expansion),
-#                     nn.BatchNorm2d(planes * block.expansion),
-#                 )
-#         layers = []
-#         layers.append(block(self.inplanes, planes, stride, downsample))
-#         self.inplanes = planes * block.expansion
-#         for _ in range(1, blocks):
-#             layers.append(block(self.inplanes, planes))
-
-#         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
 
-        #x = self.conv2(x)
-        #x = self.bn2(x)
-        #x = self.relu(x)
-        #x = self.maxpool(x)
-
-        #x = self.layer0(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
 
-#         if x.size()[-2:] == (4, 4):
-#             x = self.conv3a(x)
-#         elif x.size()[-2:] == (1, 1):
-#             x = self.conv3b(x)
-#         elif x.size()[-2:] == (2, 2):
-#             x = self.conv3c(x)
-#         elif x.size()[-2:] == (1, 2):
-#             x = self.conv3d(x)
-#         elif x.size()[-2:] == (9, 9):
-#             x = self.conv3e(x)
-#         elif x.size()[-2:] == (1, 3):
-#             x = self.conv3f(x)
-        
-#         x = self.bn3(x)
-#         x = self.relu(x)
-
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
 
-#         if self.bool_deep:
-#             x = self.relu(self.fc2(x))
         return x
 
-
-# -
 
 def resnet18(**kwargs):
     """Constructs a ResNet-18 model feature extractor.
