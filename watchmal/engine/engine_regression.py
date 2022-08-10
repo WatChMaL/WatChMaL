6b811c252818f9c9fc1cf6edd03d442311753b3d
Nick Prouse <nprouse@triumf.ca>
2022-08-10 14:11:23 -0700

    fix restoring state for regression engine

--- watchmal/engine/engine_regression.py
+++ watchmal/engine/engine_regression.py
@@ -513,7 +513,7 @@ class RegressionEngine(BaseEngine):
             self.model_accs.load_state_dict(checkpoint['state_dict'])
 
             # if optim is provided, load the state of the optim
-            if hasattr(self, 'optimizer'):
+            if self.optimizer is not None:
                 self.optimizer.load_state_dict(checkpoint['optimizer'])
 
             # load iteration count
