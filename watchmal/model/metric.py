2cf692f440b7c6a9b42dafe07974c6e5f0ccd490
Nick Prouse <n.prouse@imperial.ac.uk>
2023-11-24 14:12:06 +0000

    Unify regression and classification models (they are actually the same...) for both resnet and pointnet

--- watchmal/model/metric.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def accuracy(output, target):
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = torch.sum(pred == target).item()
-    return correct / len(target)
