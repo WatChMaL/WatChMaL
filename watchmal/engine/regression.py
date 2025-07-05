73780fbbc7c599ad7c551ddcad9aae5617a3aed3
Nick Prouse <n.prouse@imperial.ac.uk>
2025-07-05 20:52:07 +0900

    Bug fix

--- watchmal/engine/regression.py
+++ watchmal/engine/regression.py
@@ -72,7 +72,7 @@ class RegressionEngine(ReconstructionEngine):
         # split the output for each target
         split_model_out = torch.split(self.model_out, self.target_sizes, dim=1)
         self.predictions = {"predicted_" + t: o * self.scale[t] + self.offset[t]
-                            for t, o in zip(self.target_sizes.keys(), split_model_out)}
+                            for t, o in zip(self.target_key, split_model_out)}
         if self.target_dict is None:
             return self.predictions
         return self.target_dict | self.predictions
